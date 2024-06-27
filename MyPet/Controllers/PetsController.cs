d057da57c0cec8e72c4092399fb7f4fb402245c8
chathumini dasanayake <chathuminidasanayake@gmail.com>
2024-06-27 13:38:49 +0200

    Initial Commit

---
+++ MyPet/Controllers/PetsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyPet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Pets()
+        {
+            var pets = new string[] { "Pet 1", "Pet 2", "Pet 3" };
+            return Ok(pets);
+        }
+
+        [HttpPost]
+        public IActionResult NewPet() 
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdatePet()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeletePet()
+        {
+            var SomethingWentWrong = true;
+            if (SomethingWentWrong)
+                return BadRequest();
+            return Ok();
+        }
+    }
+}
